74718fe1efd7b42dde74f4cf044bb71c43a92d72
Val <val16flores@gmail.com>
2023-12-10 18:59:20 -0500

    Model y Controller
    Empleado

---
+++ Progra-Avanzada-Proyecto-API/Progra-Avanzada-Proyecto-API/Controllers/EmpleadoController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+using PrograAvanzadaProyectoAPI.Models;
+
+public class EmpleadoController : ApiController
+{
+    private MiCateringContext db = new MiCateringContext();
+
+    // GET: api/Empleados
+    public IQueryable<Empleado> GetEmpleados()
+    {
+        return db.Empleados;
+    }
+
+    // GET: api/Empleados/5
+    [ResponseType(typeof(Empleado))]
+    public IHttpActionResult GetEmpleado(int id)
+    {
+        Empleado empleado = db.Empleados.Find(id);
+        if (empleado == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(empleado);
+    }
+
+    // PUT: api/Empleados/5
+    [ResponseType(typeof(void))]
+    public IHttpActionResult PutEmpleado(int id, Empleado empleado)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        if (id != empleado.ID)
+        {
+            return BadRequest();
+        }
+
+        db.Entry(empleado).State = EntityState.Modified;
+
+        try
+        {
+            db.SaveChanges();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!EmpleadoExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return StatusCode(HttpStatusCode.NoContent);
+    }
+
+    // POST: api/Empleados
+    [ResponseType(typeof(Empleado))]
+    public IHttpActionResult PostEmpleado(Empleado empleado)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        db.Empleados.Add(empleado);
+        db.SaveChanges();
+
+        return CreatedAtRoute("DefaultApi", new { id = empleado.ID }, empleado);
+    }
+
+    // DELETE: api/Empleados/5
+    [ResponseType(typeof(Empleado))]
+    public IHttpActionResult DeleteEmpleado(int id)
+    {
+        Empleado empleado = db.Empleados.Find(id);
+        if (empleado == null)
+        {
+            return NotFound();
+        }
+
+        db.Empleados.Remove(empleado);
+        db.SaveChanges();
+
+        return Ok(empleado);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            db.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    private bool EmpleadoExists(int id)
+    {
+        return db.Empleados.Count(e => e.ID == id) > 0;
+    }
+}
---
+++ Progra-Avanzada-Proyecto-API/Progra-Avanzada-Proyecto-API/Models/EmpleadoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PrograAvanzadaProyectoAPI.Models
+{
+    public class Empleado
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string Cedula { get; set; }
+        public DateTime FechaContratacion { get; set; }
+        public DateTime? FechaSalida { get; set; }
+    }
+}
